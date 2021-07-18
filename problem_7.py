a76d95af637b38fb8af8225b81013475aa7b18d8
Prashanth K S <ksprashanth82@gmail.com>
2021-07-18 18:35:17 +0530

    Add sample test cases

--- problem_7.py
+++ problem_7.py
@@ -128,3 +128,20 @@ try:
 except AssertionError as e:
     print(e)
 # cannot override root handler
+
+# Provided example test cases
+# create the router and add a route
+router = Router("root handler", "not found handler") # remove the 'not found handler' if you did not implement this
+router.add_handler("/home/about", "about handler")  # add a route
+
+# some lookups with the expected output
+print(router.lookup("/")) 
+# should print 'root handler'
+print(router.lookup("/home")) 
+# should print 'not found handler' or None if you did not implement one
+print(router.lookup("/home/about")) 
+# should print 'about handler'
+print(router.lookup("/home/about/")) 
+# should print 'about handler' or None if you did not handle trailing slashes
+print(router.lookup("/home/about/me")) 
+# should print 'not found handler' or None if you did not implement one
